85b25743daeeafd3fa5ab257ac50c7ad6e473636
Devon Duerholz <st10043202@vcconnect.edu.za>
2022-06-04 08:42:54 +0200

    forPOE test commit

---
+++ src/test/java/com/mycompany/poe/POETest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package com.mycompany.poe;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.Test;
+
+/**
+ *
+ * @author duerh
+ */
+public class POETest {
+    
+    public POETest() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+    
+}
